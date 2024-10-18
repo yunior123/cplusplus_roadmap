7226767fa6685e5e868f61a633a63c6863c7ef7e
yunior123 <yuniorrodriguezo460@gmail.com>
2024-10-17 21:10:31 -0400

    great

---
+++ array_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string cars[5] = {
+        "Volvo", "BMW", "Ford", "Mazda", "Tesla"
+    };
+    for (int i = 0; i < 5; i++) {
+        cout << cars[i] << "\n";
+    }
+    return 0;
+}
